e765e1a83f01fd8649087f4780827359daa0157a
Mihai Semenescu <mihai.dh.semenescu@gmail.com>
2022-07-18 18:37:03 +0300

    index.html

--- index.js
+++ index.js
@@ -21,3 +21,56 @@ console.warn(mottoElement.innerHTML);
 
 mottoElement.innerHTML = mottoElement.innerHTML + " and " + jobTitle;
 //mottoElement.innerHTML += " and " + jobTitle;
+
+function showHomePage() {
+  console.info("aib");
+  document.getElementById("home").style.display = "block";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("passions").style.display = "none";
+  document.getElementById("achievements").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showSkillsPage() {
+  document.getElementById("skills").style.display = "block";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("passions").style.display = "none";
+  document.getElementById("achievements").style.display = "none";
+  document.getElementById("home").style.display = "none";
+}
+function showProjectsPage() {
+  document.getElementById("projects").style.display = "block";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("home").style.display = "none";
+  document.getElementById("passions").style.display = "none";
+  document.getElementById("achievements").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+function showLanguagesPage() {
+  document.getElementById("languages").style.display = "block";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("passions").style.display = "none";
+  document.getElementById("achievements").style.display = "none";
+}
+
+function showAchievementsPage() {
+  document.getElementById("achievements").style.display = "block";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("passions").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+function showPassionsPage() {
+  document.getElementById("passions").style.display = "block";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("home").style.display = "none";
+  document.getElementById("achievements").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+showHomePage();
